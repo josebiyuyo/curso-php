7181159d851d20a579ad895dd2c790c9335547e4
josebiyuyo <jose.biyuyo@gmail.com>
2021-07-07 16:16:40 -0400

    insercion del 1er codigo

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INDEX</title>
+</head>
+<body>
+  
+<br><br>
+<center><font color="mediumblue"><h2>LOGIN</h2></font>
+
+<form method="POST" action="uno.php" >
+    <label for="nom">
+   <b>Usuario:</b><input type="text" name="user" id="nom"><br></label>
+   <label for="contrasena">
+   <b>Contraseña:</b><input type="password" name="passw" id="contrasena" ><br><br></label>
+<input type="submit" value="ingresar">
+</form>
+</center>
+
+<?php
+
+    // include("v.php");
+?>
+</body>
+</html>
